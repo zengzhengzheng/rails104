d32096844700c7f19cd9acbdec10a98a8203cf0f
zengzheng <zengzheng2009@gmail.com>
2016-10-09 22:18:13 +0800

    check owner permission when access edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -15,11 +15,15 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permitssion."
+    end
   end
 
   def create
     @group = Group.new(group_params)
-    @group.user = current_user 
+    @group.user = current_user
 
     if @group.save
       redirect_to groups_path
